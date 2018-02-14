ebc3e3506ef69313e0dd557e94244ee617036968
Forrest Brown <forrestabrown@gmail.com>
2018-02-14 13:33:05 -0500

    feat: add webhooks example

---
+++ BitbucketServerWebHooks/BitbucketServerWebHooks/Controllers/BitbucketController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebHooks;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace GitHooksListener.Controllers
+{
+    public class BitbucketController : ControllerBase
+    {
+        [BitbucketWebHook(EventName = "pr:opened")]
+        public async Task<IActionResult> PullRequestOpened(string id, string webHookId, JObject data)
+        {
+            return this.Ok();
+        }
+    }
+}
